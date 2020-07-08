da4e5b2d51cfdb9b2c39c0f3559d9b1670a0b10c
heyimmatt <mrmcfarland@gmail.com>
2020-07-08 09:38:44 -0700

    Add logic for getting form input, making giphy api request, and console logging the response

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("Let's get this party started!");
+function getSearchTerm(evt) {
+  const input = document.querySelector('#search-text');
+  const searchTerm = input.value;
+
+  evt.preventDefault();
+  input.value = '';
+  getGiphy(searchTerm);
+}
+
+async function getGiphy(query) {
+  let res = await axios.get('http://api.giphy.com/v1/gifs/search', {params: { api_key: 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym', q: query, limit: 1, } });
+  console.log(res.data.data[0]);
+}
+
+
+
+document.querySelector('#search-btn').addEventListener('click', (evt) => {
+  getSearchTerm(evt);
+})
